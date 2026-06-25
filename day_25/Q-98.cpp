7399aad222533e8200fc18a1a36cdb1333e57d95
Nikhil Kushwah <nikhilkushwah5297@gmail.com>
2026-06-25 16:18:53 +0530

    Update Q-98.cpp

--- day_25/Q-98.cpp
+++ day_25/Q-98.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s1, s2;
+    cin >> s1;
+    cin >> s2;
+    cout << "Common Characters: ";
+    for(int i=0; i<s1.length(); i++)
+    {
+        for(int j=0; j<s2.length(); j++)
+        {
+            if(s1[i] == s2[j])
+            {
+                cout << s1[i] << " ";
+                break;
+            }
+        }
+    }
+    return 0;
+}
